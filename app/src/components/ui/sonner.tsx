91db492dcd8ebf5f4e05d4be2e05e6cf9ff5541e
Goddy01 <adigungodwin2@gmail.com>
2026-06-11 16:47:20 +0100

    updated

--- app/src/components/ui/sonner.tsx
+++ app/src/components/ui/sonner.tsx
@@ -1,8 +1,8 @@
 import {
+  CircleAlertIcon,
   CircleCheckIcon,
   InfoIcon,
   Loader2Icon,
-  OctagonXIcon,
   TriangleAlertIcon,
 } from "lucide-react"
 import { useTheme } from "next-themes"
@@ -22,7 +22,7 @@ const Toaster = ({ ...props }: ToasterProps) => {
         success: <CircleCheckIcon className="size-4" />,
         info: <InfoIcon className="size-4" />,
         warning: <TriangleAlertIcon className="size-4" />,
-        error: <OctagonXIcon className="size-4" />,
+        error: <CircleAlertIcon className="size-4" />,
         loading: <Loader2Icon className="size-4 animate-spin" />,
       }}
       style={
