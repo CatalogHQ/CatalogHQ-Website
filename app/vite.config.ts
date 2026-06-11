869dd4d46426fb274a127969e316d68b7e01aeca
Goddy01 <adigungodwin2@gmail.com>
2026-06-11 06:00:20 +0100

    updated

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       "/api": {
-        target: "http://127.0.0.1:3001",
+        target: "http://127.0.0.1:3000",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
